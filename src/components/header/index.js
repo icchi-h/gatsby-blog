cd3806d38c01be84956ffc46959e88902dcfb83f
Haruyuki Ichino <mail@icchi.me>
2019-09-19 21:26:23 +0900

    Fix: display header on post page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,19 +8,45 @@ import config from '../../config/blog-config'
 
 export default class Header extends React.Component {
   render() {
-    return (
-      <div className={styles.header_container}>
-        <Seo isRoot={true} />
-        <div className={styles.header_container__inner}>
-          <h1 className={styles.blog_title_area}>
-            <Link className={styles.blog_title} to={'/'}>
-              {config.blogTitle}
-            </Link>
-          </h1>
-          <Bio />
+    // const { location, children } = this.props
+    const { location } = this.props
+
+    let rootPath = `/`
+    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
+      rootPath = __PATH_PREFIX__ + rootPath
+    }
+    let tagPath = `/tags/`
+    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
+      tagPath = __PATH_PREFIX__ + tagPath
+    }
+    let mapPath = `/map`
+    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
+      mapPath = __PATH_PREFIX__ + mapPath
+    }
+
+    const isRoot = location ? location.pathname === rootPath : true
+    const isTag = location ? location.pathname.startsWith(tagPath) : false
+    const isMap = location ? location.pathname.startsWith(mapPath) : false
+
+    let header = null
+
+    if (isRoot || isTag || isMap) {
+      header = (
+        <div className={styles.header_container}>
+          <Seo isRoot={true} />
+          <div className={styles.header_container__inner}>
+            <h1 className={styles.blog_title_area}>
+              <Link className={styles.blog_title} to={'/'}>
+                {config.blogTitle}
+              </Link>
+            </h1>
+            <Bio />
+          </div>
+          <Rss />
         </div>
-        <Rss />
-      </div>
-    )
+      )
+    }
+
+    return header
   }
 }
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -9,12 +9,12 @@ import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
 export default class Layout extends React.Component {
   render() {
-    const { children } = this.props
+    const { location, children } = this.props
 
     return (
       <div className={styles.root_container}>
         <UserHeat />
-        <Header />
+        <Header location={location} />
         {children}
         <Footer />
       </div>
