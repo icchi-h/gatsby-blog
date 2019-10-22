9cd03cf4e6b1e0c9456116e4ddba6993ce94f949
Haruyuki Ichino <mail@icchi.me>
2019-10-22 19:53:22 +0900

    Add: pagination of each num page

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -1,10 +1,32 @@
-import { Link } from 'gatsby'
-import React from 'react'
-import styles from './index.module.scss'
+import { Link } from 'gatsby';
+import React from 'react';
+import styles from './index.module.scss';
 
 const Pagination = ({ props }) => {
-  const { pageContext } = props
-  const { previousPagePath, nextPagePath } = pageContext
+  const { pageContext } = props;
+  const { previousPagePath, nextPagePath, numberOfPages } = pageContext;
+
+  let basePath;
+  if (previousPagePath) {
+    basePath = previousPagePath.replace(/\/page\/\d$/g, '');
+  } else if (nextPagePath) {
+    basePath = nextPagePath.replace(/\/page\/\d$/g, '');
+  }
+
+  // 数字のページボタン
+  let pageNumButton = [];
+  if (numberOfPages >= 3) {
+    for (let i = 1; i <= numberOfPages; i++) {
+      const pagePath = basePath + (i !== 1 ? `/page/${i}` : '');
+      pageNumButton.push(
+        <Link to={pagePath} className={styles.pagination_pre}>
+          {i}
+        </Link>
+      );
+    }
+  } else {
+    pageNumButton = null;
+  }
 
   return (
     <div className={styles.pagination}>
@@ -13,13 +35,14 @@ const Pagination = ({ props }) => {
           前のページ
         </Link>
       ) : null}
+      {pageNumButton}
       {nextPagePath ? (
         <Link to={nextPagePath} className={styles.pagination_next}>
           次のページ
         </Link>
       ) : null}
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
