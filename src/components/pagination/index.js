2ee1cfbb7976b915e0c6b5aa0d46958559acebbe
Haruyuki Ichino <mail@icchi.me>
2019-10-23 01:13:35 +0900

    Add: elipsis pagination

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -14,15 +14,30 @@ const Pagination = ({ props }) => {
   }
 
   // 数字のページボタン
+  const maxPageNum = 5;
   let pageNumButton = [];
   if (numberOfPages >= 3) {
-    for (let i = 1; i <= numberOfPages; i++) {
-      const pagePath = basePath + (i !== 1 ? `/page/${i}` : '');
+    const ellipsisFlag = numberOfPages > maxPageNum;
+    const lastPageNum = ellipsisFlag ? numberOfPages - 2 : numberOfPages;
+    for (let i = 1; i <= lastPageNum; i++) {
+      let pagePath = basePath + (i !== 1 ? `/page/${i}` : '');
       pageNumButton.push(
-        <Link to={pagePath} className={styles.pagination_pre}>
+        <Link to={pagePath} className={styles.pagination_num}>
           {i}
         </Link>
       );
+    }
+
+    if (ellipsisFlag) {
+      pageNumButton.push(<span>...</span>);
+      for (let i = numberOfPages - 1; i <= numberOfPages; i++) {
+        let pagePath = basePath + `/page/${i}`;
+        pageNumButton.push(
+          <Link to={pagePath} className={styles.pagination_num}>
+            {i}
+          </Link>
+        );
+      }
     }
   } else {
     pageNumButton = null;
