1d83ad86148763d669d35ba5d5d2ef0c107e7fbb
Haruyuki Ichino <mail@icchi.me>
2019-10-22 16:47:45 +0900

    Update: tag-list style and refactoring

--- src/components/tag-list/index.js
+++ src/components/tag-list/index.js
@@ -1,43 +1,45 @@
-import React from 'react'
-import _get from 'lodash/get'
-import _flatMap from 'lodash/flatMap'
-import _countBy from 'lodash/countBy'
-import _orderBy from 'lodash/orderBy'
-import _map from 'lodash/map'
+import React from 'react';
+import _get from 'lodash/get';
+import _flatMap from 'lodash/flatMap';
+import _countBy from 'lodash/countBy';
+import _orderBy from 'lodash/orderBy';
+import _map from 'lodash/map';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTags } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTags } from '@fortawesome/free-solid-svg-icons';
 
-import Tag from '../tag'
-import styles from './index.module.scss'
+import Tag from '../tag';
+import styles from './index.module.scss';
 
 class TagList extends React.Component {
   render() {
-    const { posts } = this.props
+    const { posts } = this.props;
 
-    let tagCounts = _flatMap(posts, p => _get(p, 'node.fields.tags', []))
-    ;(tagCounts = _map(_countBy(tagCounts), (value, key) => {
+    let tagCounts = _flatMap(posts, p => _get(p, 'node.fields.tags', []));
+    (tagCounts = _map(_countBy(tagCounts), (value, key) => {
       return {
         name: key,
         count: value,
-      }
+      };
     })),
-      (tagCounts = _orderBy(tagCounts, ['count', 'name'], ['desc', 'asc']))
+      (tagCounts = _orderBy(tagCounts, ['count', 'name'], ['desc', 'asc']));
 
     return (
-      <div className={styles.content}>
-        <h4 className={styles.title}>
-          <FontAwesomeIcon icon={faTags} className={styles.title__icon} />
-          タグ一覧
-        </h4>
-        <div className={styles.list}>
-          {tagCounts.map(t => (
-            <Tag key={t.name} value={t.name} count={t.count} isLink />
-          ))}
+      <div className={styles.content_wrap}>
+        <div className={styles.content}>
+          <h4 className={styles.title}>
+            <FontAwesomeIcon icon={faTags} className={styles.title__icon} />
+            タグ一覧
+          </h4>
+          <div className={styles.list}>
+            {tagCounts.map(t => (
+              <Tag key={t.name} value={t.name} count={t.count} isLink />
+            ))}
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default TagList
+export default TagList;
