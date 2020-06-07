d92832681ef0df01595217467bf2bc7506181b4d
Haruyuki Ichino <mail@icchi.me>
2020-06-07 22:58:52 +0900

    refactor: 不要なスクリプトタグ要素をコメント化

--- src/components/user-heat/index.js
+++ src/components/user-heat/index.js
@@ -3,12 +3,6 @@ import { Helmet } from 'react-helmet';
 
 export class UserHeat extends React.Component {
   render() {
-    return (
-      <Helmet>
-        <script type="text/javascript">{`
-            (function(add, cla){window['UserHeatTag']=cla;window[cla]=window[cla]||function(){(window[cla].q=window[cla].q||[]).push(arguments)},window[cla].l=1*new Date();var ul=document.createElement('script');var tag = document.getElementsByTagName('script')[0];ul.async=1;ul.src=add;tag.parentNode.insertBefore(ul,tag);})('//uh.nakanohito.jp/uhj2/uh.js', '_uhtracker');_uhtracker({id:'uhxUTlOli3'});
-          `}</script>
-      </Helmet>
-    );
+    return <Helmet>{/* ここにUserHeatのスクリプトタグを挿入*/}</Helmet>;
   }
 }
